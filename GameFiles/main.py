96f65b2b3cecbbda357f431f688be96296dd8b76
Hayden <haydenogg@gmail.com>
2021-05-15 10:43:20 -0500

    added self-adjusting points to hero profile

--- GameFiles/main.py
+++ GameFiles/main.py
@@ -43,27 +43,50 @@ def play_now():
     canvas.create_text(400, 20, text="Hero Creation", font=('Candara', 25), fill='#1A5A14')
 
     def createHero():
-        # x = name.get()
-        x1 = int(health.get())
-        x2 = int(melee.get())
-        x3 = int(ranged.get())
-        x4 = int(magic.get())
-        if((x1 + x2 + x3 + x4) > 30):
+        points = 30
+        # bank = hu.bank
+
+        # gets the user input and then sets it to a temp point to subtract from points var
+        healthPoints = int(health.get())
+        points -= healthPoints
+        meleePoints = int(melee.get())
+        points -= meleePoints
+        rangedPoints = int(ranged.get())
+        points -= rangedPoints
+        magicPoints = int(magic.get())
+        points -= magicPoints
+
+        # input validation
+##      should add an elif that says "if they're all left blank do a messagebox saying please enter some stats
+        if(points < 0):
             tkinter.messagebox.showinfo("Whoops!",  "You only have 30 points. Please adjust your attributes!")
-        elif((x1<0) or (x2<0) or (x3<0) or (x4<0)):
+        elif((healthPoints<0) or (meleePoints<0) or (rangedPoints<0) or (magicPoints<0)):
             tkinter.messagebox.showinfo("Whoops!", "You cannot have a nageative value!")
-        elif(x1==0):
+        elif(healthPoints==0):
             tkinter.messagebox.showinfo("Whoops!", "Health cannot be zero!")
-        else:
+        else: # writes to file as long as all inputs are valid
             file = open('interfaceSave.txt', 'w')
             file.write(str(heroName.get()) + '\n'   + \
                        str(health.get()) + '\n'+ \
                        str(melee.get()) + '\n' + \
                        str(ranged.get()) + '\n'+ \
-                       str(magic.get()) + '\n')
+                       str(magic.get()) + '\n' + \
+                       str(points) + '\n' + \
+                       str(20))
             file.close()
             go_menu()
             tkinter.messagebox.showinfo("Congrats!", "Hero Created. Click Play Now to start your adventure!")
+
+##  info icons on right side of screen
+    # coinImage = Image.open(r'img/coinPouch.png')
+    # canvas.image = ImageTk.PhotoImage(coinImage)
+    # canvas.create_image(680,60, image=canvas.image, anchor='nw')
+
+    # coinLabel = Label(canvas, text="Coins:", fg='#1A5A14')
+    # canvas.create_window(700,95, window=coinLabel)
+
+    # coinAmount = Label(canvas, text=hu.bank, fg='#1A5A14')
+    # canvas.create_window(700,95, window=coinAmount)
     
     # take user input for user info
     heroLabel = Label(canvas, text="Hero Name:", fg='#1A5A14')
@@ -74,6 +97,7 @@ def play_now():
 
     canvas.create_text(180, 120, text="You have 30 skill points to spend. Choose wisely!", font=('Candara', 10), fill='#1A5A14')
 
+    # creates labels/input boxes for all hero atttributes
     healthLabel = Label(canvas, text="Health:", fg='#1A5A14')
     canvas.create_window(100,145, window=healthLabel)
     health = Entry(canvas)
@@ -93,8 +117,7 @@ def play_now():
     canvas.create_window(100,220, window=magicLabel)
     magic = Entry(canvas)
     canvas.create_window(200,220, window=magic)
-
-
+    
     # submit button to write all attributes to text file
     submit_button = Button(window, text="Submit", command=createHero)
     canvas.create_window(400,530, window=submit_button)
---
+++ GameFiles/testEnvironment.py
@@ -0,0 +1,88 @@
+from tkinter import *
+from win_settings import *
+import tkinter.messagebox
+
+# global window title
+window.title('TEST ENVIRONMENT')
+
+# creates canvas
+canvas = Canvas(window, width=800, height=600)
+canvas.pack(fill='both', expand=True)
+
+canvas.configure(bg='#DDE7EC')
+
+canvas.create_line(335, 800, 335, 0)
+# canvas.create_line(500, 800, 500, 0)
+        
+def createDrops():
+##  WEAPON SELECTION 
+    weaponLabel = Label(canvas, text="Select Your Weapon!")
+    canvas.create_window(20,20, window=weaponLabel, anchor='w')
+
+    # Weapon dropdown menu options
+    weapons = [
+            "Sword",
+            "Bow",
+            "Magic Staff"
+            ]
+
+    # Access the menu widget using StringVar function
+    weaponClicked = StringVar()
+              
+    # Create weapon dropdown menu
+    weaponDrop = OptionMenu(canvas, weaponClicked, *weapons)
+    canvas.create_window(150, 20, window=weaponDrop, anchor='w')
+
+##  ELEMENT SELECTION    
+    elementLabel = Label(canvas, text="Select Your Element!")
+    canvas.create_window(20, 70, window=elementLabel, anchor='w')
+            
+    # Element dropdown menu options
+    elements = [
+            "Fire",
+            "Water",
+            "Earth",
+            "Eternal"
+            ]
+
+    # Access the menu widget using StringVar function
+    elementClicked = StringVar()
+                  
+    # Create element dropdown menu
+    elementDrop = OptionMenu(canvas, elementClicked, *elements)
+    canvas.create_window(150, 70, window=elementDrop, anchor='w')
+
+    def showWeapon():
+        if(elementClicked.get() == "Eternal"):
+            tkinter.messagebox.showinfo("Ah, shucks!", "It looks like you will need to discover the Eternal element before using it to make weapons! Choose a new element.")
+        else:
+            submitLabel = Label(canvas, text="You equip a " + weaponClicked.get() + " of " + elementClicked.get())
+            canvas.create_window(150, 150, window=submitLabel, anchor='w')
+
+    submitWeapon = Button(window, text="Submit", command=showWeapon)
+    canvas.create_window(150, 110, window=submitWeapon, anchor='w')
+
+def createRads():
+    weaponLabel = Label(canvas, text="Select Your Weapon!")
+    canvas.create_window(375,20, window=weaponLabel, anchor='w')
+
+    clicked = StringVar()
+
+    swordRadio = Radiobutton(text="Sword", variable=clicked)
+    canvas.create_window(500, 20, window=swordRadio, anchor='w')
+
+    bowRadio = Radiobutton(text="Bow", variable=clicked)
+    canvas.create_window(500, 50, window=bowRadio, anchor='w')
+
+    staffRadio = Radiobutton(text="Magic Staff", variable=clicked)
+    canvas.create_window(500, 80, window=staffRadio, anchor='w')
+
+            
+    selected = clicked.get() # doesn't work yet. Selects all when hovered over
+
+def main():
+    createDrops()
+    # createRads()
+
+if __name__ == '__main__':
+    main()
